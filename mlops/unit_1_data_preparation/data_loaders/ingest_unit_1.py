8ed15884b1967a4d38457970634caf7264a55a96
moosetim <40662400+moosetim@users.noreply.github.com>
2024-06-01 15:23:01 +0000

    Adding ingest_unit_1 data loader for unit_1_data_preparation_pipeline

---
+++ mlops/unit_1_data_preparation/data_loaders/ingest_unit_1.py
@@ -0,0 +1,32 @@
+import requests
+from io import BytesIO
+from typing import List
+
+import pandas as pd
+import numpy as np
+
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+
+
+@data_loader
+def ingest_files(**kwargs) -> pd.DataFrame:
+    dfs: List[pd.DataFrame] = []
+
+    for year, months in [(2024, (1, 3))]:
+        for i in range(*months):
+            response = requests.get(
+                'https://github.com/mage-ai/datasets/raw/master/taxi/green'
+                f'/{year}/{i:02d}.parquet'
+            )
+
+            if response.status_code != 200:
+                raise Exception(response.text)
+
+            df = pd.read_parquet(BytesIO(response.content))
+            # if time series chart on mage error, add code below
+            df['lpep_pickup_datetime_cleaned'] = df['lpep_pickup_datetime'].astype(np.int64) // 10**9
+            dfs.append(df)
+
+
+    return pd.concat(dfs)
